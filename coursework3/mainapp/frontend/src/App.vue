d15ac5bbf14791c06923be3af6d4fe4982418203
tazdg <71644246+tazdg@users.noreply.github.com>
2022-12-03 14:22:53 +0000

    Bootstrap and Vite

--- coursework3/mainapp/frontend/src/App.vue
+++ coursework3/mainapp/frontend/src/App.vue
@@ -9,8 +9,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a  href="https://vuejs.org/" target="_blank">
-      <img  src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
--- coursework3/mainapp/frontend/src/main.js
+++ coursework3/mainapp/frontend/src/main.js
@@ -3,4 +3,5 @@ import './style.css'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+
 createApp(App).mount('#app')
