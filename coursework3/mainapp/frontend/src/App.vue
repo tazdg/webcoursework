f38987b0b8c7323b7b4d0b84b91c9f1c4cbf9c50
tazdg <71644246+tazdg@users.noreply.github.com>
2022-12-03 16:37:11 +0000

    App.vue and Component setup

--- coursework3/mainapp/frontend/src/App.vue
+++ coursework3/mainapp/frontend/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
     Welcome to {{app_name}} <!-- The variable app_name is taken from script below -->
-    
+    <Profile/>
   </div>
 </template>
 
 
 <script>
+import Profile from './components/Profile.vue'
   export default{
+
+    components:{
+      Profile,
+    },
     data(){
       return{
         app_name: 'Auction Genie'
       }
-    }
+    },
+
+    components: { Profile }
   }
 </script>
