8111cd2990309c99adce8045c23e4e9bc1304bf1
tazdg <71644246+tazdg@users.noreply.github.com>
2022-12-13 10:29:37 +0000

    Search Bar
    Added search bar that Laurencia created. Added searching description and changed styling slightly.

--- coursework3/mainapp/frontend/src/App.vue
+++ coursework3/mainapp/frontend/src/App.vue
@@ -7,7 +7,7 @@
 </head>
 
 <div>
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
+<nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
   <a class="navbar-brand" href="#">Auction Genie</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
--- coursework3/mainapp/frontend/src/components/Auctions.vue
+++ coursework3/mainapp/frontend/src/components/Auctions.vue
@@ -7,6 +7,17 @@
     </div>
 
     <div>
+      <!-- Search Bar -->
+      <h6 class="mt-5">Search</h6>
+      <div class="input-group mb-5 justify-content-center">
+        <input
+          class="bg-light text-dark rounded w-50 border border-info border-3"
+          type="text"
+          v-model="search"
+          placeholder="Search items by title or description..."
+        />
+      </div>
+
         <button class="rounded" @click="fetch_items">Fetch items (remove button later)</button>
         <table class="table table-bordered">
         <tr>
@@ -20,12 +31,10 @@
           <th></th>
         </tr>
 
-        <tr v-for="item in items">
+        <tr v-for="item in filteredItems">
           <td>{{item.title}}</td>
-
           <td>{{item.description}}</td>
           <td><a href="http://localhost:8000{{item.image}}"><img :src="'http://localhost:8000'+item.image" width="140" height="140"/></a></td>
-
           <td>£{{item.starting_price}}</td>
           <td>Auction Ends: {{item.date_ends}}</td>
           <td v-if="item.available">(Available)</td>
@@ -44,6 +53,7 @@ export default{
     data() {
         return {
             items: [],
+            search: ''
         };
     },
     mounted() {
@@ -60,27 +70,38 @@ export default{
         //     this.items = data.items;
         // },
 
-        async fetch_items() {
-            //Perform Ajax request to fetch list of items
-            console.log(`fetching..`)
-            let response = await fetch("http://localhost:8000/mainapp/api/items/",
-            {
-              credentials: 
-              "include",
+      async fetch_items() {
+        //Perform Ajax request to fetch list of items
+        console.log(`fetching..`)
+        let response = await fetch("http://localhost:8000/mainapp/api/items/",
+        {
+          credentials: 
+          "include",
 
-              mode: 
-              "cors",
+          mode: 
+          "cors",
 
-              referrerPolicy: 
-              "no-referrer",
-            }
+          referrerPolicy: 
+          "no-referrer",
+        }
 
-            
-            );
-            let data = await response.json();
-            this.items = data.items;
-        },
+        );
+        let data = await response.json();
+        this.items = data.items;
+      },
     },
+
+    computed: {
+      filteredItems(){
+        let filteredItems = this.items.filter((item) => {
+          return item.title.toLowerCase().includes(this.search.toLowerCase()) + item.description.toLowerCase().includes(this.search.toLowerCase());
+        })
+        let orderedItems = filteredItems.sort((a,b) => {
+          return b.upvoted - a.upvoted;
+        })        
+        return orderedItems;
+      }
+    }
 
 }
 </script>
